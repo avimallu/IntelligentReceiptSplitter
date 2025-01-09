bd8182cb53bf9a2640a78dd2dd5401655e0ecada
Avinash Mallya <avimallu@protonmail.ch>
2025-01-08 18:32:11 -0600

    Update simple OCR test

--- tests/test_surya.py
+++ tests/test_surya.py
@@ -1,7 +1,9 @@
 from src.ocr.surya import SuryaOCR
+from PIL.Image import open as pil_open
 
 
 def test_ocr_sample_image():
-    image_path = "data/pytest/image_upload_test.png"
-    output = ocr_image(image_path=image_path)
-    assert output is not None
+    image_path = pil_open("assets/pytest/OCR_test.png")
+    ocr = SuryaOCR()
+    output = ocr.ordered_ocr_text(image_path)
+    assert output == "This is Line 1\nThis is Line 2\nThis is Line 5"
